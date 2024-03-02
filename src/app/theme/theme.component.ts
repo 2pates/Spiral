52de5bd155e129a9d046dd25026cb68247089962
linasa <lina.b4007@gmail.com>
2024-03-02 16:05:53 +0100

    refactor theme : gestion texte long (titre & contenu)
    Changement taille des titres de blocs
    Coupure du texte affiché si trop long (dépasse nblignes défini)

--- src/app/theme/theme.component.ts
+++ src/app/theme/theme.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-theme',
   templateUrl: './theme.component.html',
-  styleUrl: './theme.component.css'
+  styleUrls: ['./theme.component.css']
 })
-export class ThemeComponent {
+export class ThemeComponent implements AfterViewInit {
+  @ViewChildren('title') titles!: QueryList<ElementRef>;
 
+  constructor() {
+    this.titles = new QueryList<ElementRef>();
+  }
+
+  ngAfterViewInit() {
+    this.titles.forEach(title => this.adjustFontSize(title.nativeElement));
+  }
+
+  adjustFontSize(element: HTMLElement) {
+    var textLength = element.textContent?.length ?? 0
+    var fontSizeMax = 2.8;
+    var fontSizeMin = 1.5;
+    var fontSize = fontSizeMax-0.03*textLength;
+    if (fontSize < fontSizeMin) {
+      fontSize = fontSizeMin;
+    }
+    var fontSizeFinal = fontSize + "rem";
+    element.style.fontSize = fontSizeFinal;
+  }
 }
